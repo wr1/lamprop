e3980fee18f6d4c680beb89cd44da3d7b411c0c9
wr1 <8971152+wr1@users.noreply.github.com>
2025-12-10 11:38:39 +0100

    move convert

--- examples/convert_lamprop.py
+++ examples/convert_lamprop.py
@@ -1,13 +1,6 @@
-# file: convert-lamprop.py
-#
-# Author: R.F. Smith <rsmith@xs4all.nl>
-# Created: 2017-04-16 10:38:30 +0200
-# Last modified: 2018-04-17T21:28:34+0200
-
 """Script to convert lamprop files from the old style fiber parameters
 (E1, E2, v12, G12, cte1, cte2, density) to the new style parameters
 (E1, ν12, α1, ρ)."""
-
 import argparse
 import logging
 import shutil
@@ -35,36 +28,41 @@ SOFTWARE."""
 
 
 class LicenseAction(argparse.Action):
+    """Action to print license and exit."""
     def __call__(self, parser, namespace, values, option_string=None):
+        print(_lic)
         sys.exit()
 
 
 def readlines(path):
     """Read a file into a list of lines."""
-    with open(path) as f:
+    with open(path, encoding="utf-8") as f:
         return f.readlines()
 
 
 def oldstyle_fibers(lines):
+    """Find lines with old style fiber definitions."""
     flines = ((num, ln) for num, ln in enumerate(lines) if ln.strip().startswith("f:"))
     oldstyle = []
     for num, ln in flines:
         items = ln.split()
         try:
-            item5 = float(items[5])  # noqa
+            float(items[5])
             oldstyle.append(num)
-        except ValueError:
+        except (ValueError, IndexError):
             # new style, do nothing
             pass
     return oldstyle
 
 
 def changeline(ln):
+    """Change old style line to new style."""
     f, E1, _, v12, _, alpha1, _, rho, name = ln.split(maxsplit=8)
     return f"{f} {E1} {v12} {alpha1} {rho} {name}"
 
 
 def main(argv):
+    """Main function."""
     # Process the command-line arguments
     opts = argparse.ArgumentParser(prog="convert-lamprop", description=__doc__)
     opts.add_argument(
@@ -85,7 +83,7 @@ def main(argv):
         format="%(levelname)s: %(message)s",
     )
     for path in args.files:
-        if path[-4:] != ".lam":
+        if not path.endswith(".lam"):
             logging.error(f"{path} is not a lamprop file; skipping")
             continue
         try:
@@ -105,7 +103,7 @@ def main(argv):
             for n in oldidx:
                 lines[n] = changeline(lines[n])
             # Write the file.
-            with open(path, "w") as f:
+            with open(path, "w", encoding="utf-8") as f:
                 f.writelines(lines)
         else:
             logging.info(f"skipping {path}; no old style lines")
